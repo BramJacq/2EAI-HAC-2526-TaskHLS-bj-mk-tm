891f07ac186a857c23d72d48cfa50bdffc3a9105
Maciej-Klobukowski <12401667@student.pxl.be>
2026-05-23 05:16:41 +0000

    Add project verification script

---
+++ scripts/check_project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Searching HLS reports..."
+find . -name "*.rpt"
+
+echo "Searching TCL scripts..."
+find . -name "*.tcl"
+
+echo "Searching bitstreams..."
+find . -name "*.bit"
